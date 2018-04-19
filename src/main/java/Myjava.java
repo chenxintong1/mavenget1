564014d8c4f5de3423a8abdc1612ca28886a206b
chenxintong <244239019@qq.com>
2018-04-20 01:08:43 +0800

    idea整合git

---
+++ src/main/java/Myjava.java
@@ -0,0 +1,13 @@
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ *
+ * @author: 24423
+ * Date: 2018-04-20
+ * Time: 1:06
+ */
+public class Myjava {
+    public static void main(String[] args) {
+        System.out.println("aaaaa");
+    }
+}
