23e7a7e10f871c5522a5d5bd960522389c2cdb57
chenxintong1 <38597285+chenxintong1@users.noreply.github.com>
2018-04-22 10:03:01 +0800

    Update Myjava.java
    update

--- src/main/java/Myjava.java
+++ src/main/java/Myjava.java
@@ -6,11 +6,4 @@
  * Date: 2018-04-20
  * Time: 1:06
  */
-public class Myjava {
-    public static void main(String[] args) {
-        System.out.println("aaaaa");
-        System.out.println("efefefef");
-        System.out.println("ccccccc");
-        System.out.println("c");
-    }
-}
+
